b6cedbfbe337c339f4f625b6ad9837b342dcb0f7
Mohey Eldeen Maher <54249650+mohey-e-maher@users.noreply.github.com>
2024-06-22 07:41:19 +0300

    updates to finilaize

--- js/js_m.js
+++ js/js_m.js
@@ -49,23 +49,6 @@ document.addEventListener('scroll', function () {
 });
 
 
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var moreText = document.getElementById('moreText');
-    var text = document.getElementById('text');
-    var button = document.getElementById('toggleButton');
-
-    if (moreText.style.display === 'none' || moreText.style.display === '') {
-        moreText.style.display = 'inline';
-        text.style.display = 'block';
-        text.style.webkitLineClamp = 'unset';
-        button.textContent = 'Read less';
-    } else {
-        moreText.style.display = 'none';
-        text.style.webkitLineClamp = '3';
-        button.textContent = 'Read more';
-    }
-});
-
 //////////////////////////////// image - slider //////////////////////////////////
   /**
    * Testimonials slider
